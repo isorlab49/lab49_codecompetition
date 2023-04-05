9e879cad4991117686956534d0dd02c44b28e04b
isor <istvan.ratkovszki@iongroup.com>
2023-04-05 10:48:10 +0200

    Adds minimal solutions after march competition was done.

---
+++ 03_fizzbuzz/java/src/M.java
@@ -0,0 +1,1 @@
+class M{public static void main(String[]a){for(int i=0;i++<100;System.out.println((i%3>0?"":"Fizz")+(i%5>0?i%3>0?i:"":"Buzz")))}}
---
+++ 03_fizzbuzz/python/fizzbuzz_min.py
@@ -0,0 +1,1 @@
+for a in range(100):print('Fizz'*(a%3//2)+'Buzz'*(a%5//4) or a+1)
